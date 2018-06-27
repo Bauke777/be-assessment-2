14e3fd34794e8a6fcd949cf4632ac12f70b9fcd6
Bauke Posthuma <baukeposthuma@gmail.com>
2018-06-28 00:00:16 +0200

    Change boot message

--- database/database.js
+++ database/database.js
@@ -5,5 +5,6 @@ mongoose.connect('mongodb://localhost/date_database');
 var mdb = mongoose.connection;
 mdb.on('error', console.error.bind(console, 'connection error:'));
 mdb.once('open', function() {
-  console.log('connected to mongodb')
+  console.log('Connected to mongodb')
+  console.log('Your application is running on http://localhost:1950/login')
 });
